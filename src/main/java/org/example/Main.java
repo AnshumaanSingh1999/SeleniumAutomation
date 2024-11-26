fd2b6a6af55e99994d2a1e98c2d234b5e714d85c
Anshumaan Singh <47323148+AnshumaanSingh1999@users.noreply.github.com>
2024-11-26 12:36:53 +0530

    modification
    modification

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,30 +6,36 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
 
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.out.print("Automation Task Running");
 
-        System.setProperty("webdriver.chrome.driver","C:\\Users\\HP\\Downloads\\SeleniumAutomation\\src\\main\\resources\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver","src/main/resources/chromedriver.exe");
 
         WebDriver driver = new ChromeDriver();
 
         driver.manage().window().maximize();
 
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
 
         driver.get("http://localhost:4200/");
 
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+        Thread.sleep(2000);
+
+
+
 
 
         WebElement p=driver.findElement(By.xpath("/html/body/app-root/app-login/button"));
 
         //p.sendKeys("Selenium Java");
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+        Thread.sleep(2000);
 
 
         p.click();
+
+        Thread.sleep(2000);
+
 
 
 
